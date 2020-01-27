f7b1051e31e86f844d2afac7b4b9be9e8f386677
gortaina <joel.codigo@gmail.com>
2020-01-26 21:20:51 -0300

    :fire:

--- src/models/User_old12.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// com o mongoose ele facilita a manipulação de dados do mongoDB
-// como create, delete, update, pesquisa
-const UserSchema = new mongoose.Schema({
-    email: String,
-});
-
-module.exports = mongoose.model('User', UserSchema);
